40572196e764fc467c5ab13cbd55330fa77e7eb2
TsaqifArkan <scalled040201@gmail.com>
2025-05-28 09:41:17 +0700

    PlayAroundw/Laravel

---
+++ resources/views/about.blade.php
@@ -0,0 +1,5 @@
+<x-layout>
+    <x-slot:titles>{{ $title }}</x-slot>
+        <h3 class="text-xl">Ini Adalah Halaman About</h3>
+        <h3>Owner : {{ $name }}</h3>
+</x-layout>
--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,14 @@
 
 use Illuminate\Support\Facades\Route;
 
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
 Route::get('/', function () {
-    return view('welcome');
+    return view('home', ['title' => 'Home Page']);
+});
+
+Route::get('/about', function () {
+    return view('about', ['name' => 'Tsaqif Muhammad Arkan', 'title' => 'About']);
 });
