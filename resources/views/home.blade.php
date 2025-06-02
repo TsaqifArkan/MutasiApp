f4eb358bc431f1c001dbdb893aa1b49721133a9d
TsaqifArkan <scalled040201@gmail.com>
2025-06-02 13:52:41 +0700

    MinorBugActiveTabnImplmntDebugBar

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,4 +1,18 @@
 <x-layout>
     <x-slot:titles>{{ $title }}</x-slot>
         <h3 class="text-xl">Ini Adalah Halaman Home Page</h3>
+        <article>
+            <h2>Contoh Judul</h2>
+            <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Nulla aut, fuga ratione illum error sapiente?
+                Neque beatae autem in velit numquam, id aliquid praesentium! Voluptate asperiores odit accusamus
+                voluptatibus mollitia.
+                Beatae, impedit? Consequuntur quaerat commodi eligendi neque magnam excepturi iusto laudantium, libero
+                illo corrupti sit soluta, nulla eveniet voluptates tempora quo eius voluptate optio error beatae
+                mollitia. Nam, similique nostrum?
+                Veritatis nulla inventore illum libero saepe vel assumenda magnam similique unde ea cupiditate, soluta
+                est dolor, provident adipisci tempore at cumque repudiandae repellendus quibusdam accusantium quia?
+                Molestias in facere ex.
+            </p>
+        </article>
 </x-layout>
