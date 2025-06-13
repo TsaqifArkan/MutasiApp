006780ecbd223f639740a538e5222d5253159e8a
TsaqifArkan <scalled040201@gmail.com>
2025-06-13 16:42:21 +0700

    MinorFixMobileLayout

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -9,7 +9,6 @@
                 </div>
                 <div class="hidden md:block">
                     <div class="ml-10 flex items-baseline space-x-4">
-                    <div class="ml-10 flex items-baseline space-x-4">
                         <x-nav-link href="/" :active="request()->is('/')">Home</x-nav-link>
                         <x-nav-link href="/all-sk" :active="request()->is('all-sk')">Seluruh SK</x-nav-link>
                         <x-nav-link href="/jen-jabs" :active="request()->is('jen-jabs')">Jenis Jabatan</x-nav-link>
@@ -31,7 +30,7 @@
             </div>
             <div class="hidden md:block">
                 <div class="ml-4 flex items-center md:ml-6">
-                    <button type="button"
+                    {{-- <button type="button"
                         class="relative rounded-full bg-gray-800 p-1 text-gray-400 hover:text-white focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800 focus:outline-hidden">
                         <span class="absolute -inset-1.5"></span>
                         <span class="sr-only">View notifications</span>
@@ -40,7 +39,7 @@
                             <path stroke-linecap="round" stroke-linejoin="round"
                                 d="M14.857 17.082a23.848 23.848 0 0 0 5.454-1.31A8.967 8.967 0 0 1 18 9.75V9A6 6 0 0 0 6 9v.75a8.967 8.967 0 0 1-2.312 6.022c1.733.64 3.56 1.085 5.455 1.31m5.714 0a24.255 24.255 0 0 1-5.714 0m5.714 0a3 3 0 1 1-5.714 0" />
                         </svg>
-                    </button>
+                    </button> --}}
 
                     <!-- Profile dropdown -->
                     <div class="relative ml-3">
@@ -117,6 +116,9 @@
         <div class="space-y-1 px-2 pt-2 pb-3 sm:px-3">
             <!-- Current: "bg-gray-900 text-white", Default: "text-gray-300 hover:bg-gray-700 hover:text-white" -->
             <x-nav-link href="/" :active="request()->is('/')">Home</x-nav-link>
+            <x-nav-link href="/all-sk" :active="request()->is('all-sk')">Seluruh SK</x-nav-link>
+            <x-nav-link href="/jen-jabs" :active="request()->is('jen-jabs')">Jenis Jabatan</x-nav-link>
+            <x-nav-link href="/sk-jab" :active="request()->is('sk-jab')">SK Record+Jabatan</x-nav-link>
             <x-nav-link href="/about" :active="request()->is('about')">About</x-nav-link>
             {{-- <a href="#" class="block rounded-md bg-gray-900 px-3 py-2 text-base font-medium text-white"
                 aria-current="page">Dashboard</a>
@@ -141,7 +143,7 @@
                     <div class="text-base/5 font-medium text-white">Tom Cook</div>
                     <div class="text-sm font-medium text-gray-400">tom@example.com</div>
                 </div>
-                <button type="button"
+                {{-- <button type="button"
                     class="relative ml-auto shrink-0 rounded-full bg-gray-800 p-1 text-gray-400 hover:text-white focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800 focus:outline-hidden">
                     <span class="absolute -inset-1.5"></span>
                     <span class="sr-only">View notifications</span>
@@ -150,7 +152,7 @@
                         <path stroke-linecap="round" stroke-linejoin="round"
                             d="M14.857 17.082a23.848 23.848 0 0 0 5.454-1.31A8.967 8.967 0 0 1 18 9.75V9A6 6 0 0 0 6 9v.75a8.967 8.967 0 0 1-2.312 6.022c1.733.64 3.56 1.085 5.455 1.31m5.714 0a24.255 24.255 0 0 1-5.714 0m5.714 0a3 3 0 1 1-5.714 0" />
                     </svg>
-                </button>
+                </button> --}}
             </div>
             <div class="mt-3 space-y-1 px-2">
                 <a href="#"
