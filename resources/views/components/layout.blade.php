f4346b5b1b13325a4afb91384f60bccb463c1e5c
TsaqifArkan <scalled040201@gmail.com>
2025-10-06 19:34:49 +0700

    LastCommitB4FullMigrate2Bootstrap

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -14,14 +14,6 @@
 
 <body class="h-full">
 
-    <!--
-  This example requires updating your template:
-
-  ```
-  <html class="h-full bg-gray-100">
-  <body class="h-full">
-  ```
--->
     <div class="min-h-full">
         <x-navbar></x-navbar>
         <x-header>{{ $titles }}</x-header>
