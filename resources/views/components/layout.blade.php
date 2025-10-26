9022197472a1c99ebbd0e25395100f3f12d169cb
TsaqifArkan <scalled040201@gmail.com>
2025-10-26 19:23:38 +0700

    MigrateFromNavbarTemplateToMyOwn

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -1,5 +1,5 @@
-<!DOCTYPE html class="h-full bg-gray-100">
-<html lang="en">
+<!DOCTYPE html>
+<html lang="en" data-theme="light">
 
 <head>
     <meta charset="UTF-8">
@@ -12,11 +12,28 @@
     <title>{{ $titles }}</title>
 </head>
 
-<body class="h-full">
+<body class="min-h-screen flex flex-col">
 
     <div class="min-h-full">
         <x-navbar></x-navbar>
-        <x-header>{{ $titles }}</x-header>
+        {{-- <x-header>{{ $titles }}</x-header> --}}
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium ipsam debitis voluptates. Aspernatur
+        aliquid dolores, voluptatum sed quod dolorem totam quisquam! Sint at possimus id quos sapiente natus iste ea!
+        Deserunt voluptates laboriosam aliquid dicta excepturi nam. Aperiam voluptatibus sit facere dolorem nam dolores
+        delectus. In quia dignissimos eum temporibus. Consequatur, error. Veritatis quis repudiandae dolorem expedita
+        non ea eaque?
+        Quod consequatur accusamus quis? Assumenda, dolorum molestias modi neque veritatis sunt doloremque quae, debitis
+        maxime repellendus temporibus? Odio placeat eligendi saepe pariatur voluptates dolores modi corporis! Alias
+        minima reprehenderit laboriosam.
+        Atque fugit doloremque debitis maxime nulla aut tempora suscipit libero, dicta quo eos consequatur quod, qui
+        aperiam iusto quibusdam. Doloremque fugiat voluptatum voluptatibus fuga similique tempora deleniti consequuntur
+        natus officiis.
+        Qui obcaecati veritatis labore dicta ipsa impedit accusamus vel, quam praesentium et quidem deleniti fugiat quo,
+        commodi voluptates molestias natus architecto voluptate, possimus aliquam eligendi doloribus? Voluptates quaerat
+        fugit rem?
+        <div class="p-8">
+            <button class="btn btn-info">Hello DaisyUI 🌸</button>
+        </div>
 
         <main>
             <div class="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -1,164 +1,48 @@
-<nav class="bg-gray-800" x-data="{ isOpen: false }">
-    <div class="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
+<nav class="navbar bg-[#CBCBCB] fixed top-0 left-0 right-0 z-50 p-0.5 shadow-md" x-data="{ isOpen: false }">
+    <div class="mx-auto px-2 sm:px-4 lg:px-6 w-full">
         <div class="flex h-16 items-center justify-between">
-            <div class="flex items-center">
-                <div class="shrink-0">
-                    <img class="size-8"
-                        src="https://tailwindcss.com/plus-assets/img/logos/mark.svg?color=indigo&shade=500"
-                        alt="Your Company">
-                </div>
-                <div class="hidden md:block">
-                    <div class="ml-10 flex items-baseline space-x-4">
-                        <x-nav-link href="/" :active="request()->is('/')">Home</x-nav-link>
-                        <x-nav-link href="/sk-rec" :active="request()->is('sk-rec')">Seluruh SK</x-nav-link>
-                        <x-nav-link href="/jen-sk" :active="request()->is('jen-sk')">Jenis SK</x-nav-link>
-                        <x-nav-link href="/gol" :active="request()->is('gol')">Golongan ASN</x-nav-link>
-                        <x-nav-link href="/jen-jab" :active="request()->is('jen-jab')">Jabatan Struktural</x-nav-link>
-                        <x-nav-link href="/about" :active="request()->is('about')">About</x-nav-link>
-                        {{-- <!-- Current: "bg-gray-900 text-white", Default: "text-gray-300 hover:bg-gray-700 hover:text-white" -->
-                        <a href="/" class="rounded-md bg-gray-900 px-3 py-2 text-sm font-medium text-white"
-                            aria-current="page">Dashboard</a>
-                        <a href="/about"
-                            class="rounded-md px-3 py-2 text-sm font-medium text-gray-300 hover:bg-gray-700 hover:text-white">Team</a>
-                        <a href="#"
-                            class="rounded-md px-3 py-2 text-sm font-medium text-gray-300 hover:bg-gray-700 hover:text-white">Projects</a>
-                        <a href="#"
-                            class="rounded-md px-3 py-2 text-sm font-medium text-gray-300 hover:bg-gray-700 hover:text-white">Calendar</a>
-                        <a href="#"
-                            class="rounded-md px-3 py-2 text-sm font-medium text-gray-300 hover:bg-gray-700 hover:text-white">Reports</a> --}}
-                    </div>
-                </div>
+            <!-- LEFT SIDE: Logo -->
+            <div class="flex items-center gap-2">
+                <a href="/" class="flex items-center gap-2">
+                    <img src="{{ asset('img/bpkp_logo.png') }}" alt="Logo" class="w-auto h-8" />
+                    <span class="text-xl font-bold pl-2">MutasiApp</span>
+                </a>
             </div>
-            <div class="hidden md:block">
-                <div class="ml-4 flex items-center md:ml-6">
-                    {{-- <button type="button"
-                        class="relative rounded-full bg-gray-800 p-1 text-gray-400 hover:text-white focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800 focus:outline-hidden">
-                        <span class="absolute -inset-1.5"></span>
-                        <span class="sr-only">View notifications</span>
-                        <svg class="size-6" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
-                            stroke="currentColor" aria-hidden="true" data-slot="icon">
-                            <path stroke-linecap="round" stroke-linejoin="round"
-                                d="M14.857 17.082a23.848 23.848 0 0 0 5.454-1.31A8.967 8.967 0 0 1 18 9.75V9A6 6 0 0 0 6 9v.75a8.967 8.967 0 0 1-2.312 6.022c1.733.64 3.56 1.085 5.455 1.31m5.714 0a24.255 24.255 0 0 1-5.714 0m5.714 0a3 3 0 1 1-5.714 0" />
-                        </svg>
-                    </button> --}}
-
-                    <!-- Profile dropdown -->
-                    <div class="relative ml-3">
-                        <div>
-                            <button type="button" @click="isOpen = !isOpen"
-                                class="relative flex max-w-xs items-center rounded-full bg-gray-800 text-sm focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800 focus:outline-hidden"
-                                id="user-menu-button" aria-expanded="false" aria-haspopup="true">
-                                <span class="absolute -inset-1.5"></span>
-                                <span class="sr-only">Open user menu</span>
-                                <img class="size-8 rounded-full" {{-- src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80" --}}
-                                    src="{{ asset('img/defusr.png') }}" alt="user-profile-icon">
-                            </button>
-                        </div>
-
-                        <!--
-      Dropdown menu, show/hide based on menu state.
-
-      Entering: "transition ease-out duration-100"
-        From: "transform opacity-0 scale-95"
-        To: "transform opacity-100 scale-100"
-      Leaving: "transition ease-in duration-75"
-        From: "transform opacity-100 scale-100"
-        To: "transform opacity-0 scale-95"
-    -->
-                        <div x-show="isOpen" x-transition:enter="transition ease-out duration-100 transform"
-                            x-transition:enter-start="opacity-0 scale-95" x-transition:enter-end="opacity-100 scale-100"
-                            x-transition:leave="transition ease-in duration-75 transform"
-                            x-transition:leave-start="opacity-100 scale-100" x-transition:leave-end="opacity-0 scale-95"
-                            class="absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black/5 focus:outline-hidden"
-                            role="menu" aria-orientation="vertical" aria-labelledby="user-menu-button"
-                            tabindex="-1">
-                            <!-- Active: "bg-gray-100 outline-hidden", Not Active: "" -->
-                            <a href="#" class="block px-4 py-2 text-sm text-gray-700" role="menuitem"
-                                tabindex="-1" id="user-menu-item-0">Your Profile</a>
-                            <a href="#" class="block px-4 py-2 text-sm text-gray-700" role="menuitem"
-                                tabindex="-1" id="user-menu-item-1">Settings</a>
-                            <a href="#" class="block px-4 py-2 text-sm text-gray-700" role="menuitem"
-                                tabindex="-1" id="user-menu-item-2">Sign out</a>
-                        </div>
-                    </div>
-                </div>
-            </div>
-            <div class="-mr-2 flex md:hidden">
-                <!-- Mobile menu button -->
-                <button type="button" @click="isOpen = !isOpen"
-                    class="relative inline-flex items-center justify-center rounded-md bg-gray-800 p-2 text-gray-400 hover:bg-gray-700 hover:text-white focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800 focus:outline-hidden"
-                    aria-controls="mobile-menu" aria-expanded="false">
-                    <span class="absolute -inset-0.5"></span>
-                    <span class="sr-only">Open main menu</span>
-                    <!-- Menu open: "hidden", Menu closed: "block" -->
-                    <svg :class="{ 'hidden': isOpen, 'block': !isOpen }" class="block size-6" fill="none"
-                        viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" aria-hidden="true"
-                        data-slot="icon">
-                        <path stroke-linecap="round" stroke-linejoin="round"
-                            d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
-                    </svg>
-                    <!-- Menu open: "block", Menu closed: "hidden" -->
-                    <svg :class="{ 'block': isOpen, 'hidden': !isOpen }" class="hidden size-6" fill="none"
-                        viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" aria-hidden="true"
-                        data-slot="icon">
-                        <path stroke-linecap="round" stroke-linejoin="round" d="M6 18 18 6M6 6l12 12" />
+            <!-- RIGHT SIDE: Actions -->
+            {{-- <div class="flex items-center gap-3">
+                <!-- Search Button -->
+                <button class="btn btn-ghost btn-circle">
+                    <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" fill="none" viewBox="0 0 24 24"
+                        stroke="currentColor">
+                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                            d="m21 21-4.35-4.35m0 0a7.5 7.5 0 1 0-10.6-10.6 7.5 7.5 0 0 0 10.6 10.6z" />
                     </svg>
                 </button>
-            </div>
-        </div>
-    </div>
-
-    <!-- Mobile menu, show/hide based on menu state. -->
-    <div x-show="isOpen" class="md:hidden" id="mobile-menu">
-        <div class="space-y-1 px-2 pt-2 pb-3 sm:px-3">
-            <!-- Current: "bg-gray-900 text-white", Default: "text-gray-300 hover:bg-gray-700 hover:text-white" -->
-            <x-nav-link href="/" :active="request()->is('/')">Home</x-nav-link>
-            <x-nav-link href="/sk-rec" :active="request()->is('sk-rec')">Seluruh SK</x-nav-link>
-            <x-nav-link href="/jen-sk" :active="request()->is('jen-sk')">Jenis SK</x-nav-link>
-            <x-nav-link href="/gol" :active="request()->is('gol')">Golongan ASN</x-nav-link>
-            <x-nav-link href="/jen-jab" :active="request()->is('jen-jab')">Jabatan Struktural</x-nav-link>
-            <x-nav-link href="/about" :active="request()->is('about')">About</x-nav-link>
-            {{-- <a href="#" class="block rounded-md bg-gray-900 px-3 py-2 text-base font-medium text-white"
-                aria-current="page">Dashboard</a>
-            <a href="#"
-                class="block rounded-md px-3 py-2 text-base font-medium text-gray-300 hover:bg-gray-700 hover:text-white">Team</a>
-            <a href="#"
-                class="block rounded-md px-3 py-2 text-base font-medium text-gray-300 hover:bg-gray-700 hover:text-white">Projects</a>
-            <a href="#"
-                class="block rounded-md px-3 py-2 text-base font-medium text-gray-300 hover:bg-gray-700 hover:text-white">Calendar</a>
-            <a href="#"
-                class="block rounded-md px-3 py-2 text-base font-medium text-gray-300 hover:bg-gray-700 hover:text-white">Reports</a> --}}
-        </div>
-        <div class="border-t border-gray-700 pt-4 pb-3">
-            <div class="flex items-center px-5">
-                <div class="shrink-0">
-                    <img class="size-10 rounded-full" {{-- src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80" --}} src="{{ asset('img/defusr.png') }}"
-                        alt="user-profile-icon">
-                </div>
-                <div class="ml-3">
-                    <div class="text-base/5 font-medium text-white">Aveneraa</div>
-                    <div class="text-sm font-medium text-gray-400">tom@example.com</div>
-                </div>
-                {{-- <button type="button"
-                    class="relative ml-auto shrink-0 rounded-full bg-gray-800 p-1 text-gray-400 hover:text-white focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800 focus:outline-hidden">
-                    <span class="absolute -inset-1.5"></span>
-                    <span class="sr-only">View notifications</span>
-                    <svg class="size-6" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor"
-                        aria-hidden="true" data-slot="icon">
-                        <path stroke-linecap="round" stroke-linejoin="round"
-                            d="M14.857 17.082a23.848 23.848 0 0 0 5.454-1.31A8.967 8.967 0 0 1 18 9.75V9A6 6 0 0 0 6 9v.75a8.967 8.967 0 0 1-2.312 6.022c1.733.64 3.56 1.085 5.455 1.31m5.714 0a24.255 24.255 0 0 1-5.714 0m5.714 0a3 3 0 1 1-5.714 0" />
+            </div> --}}
+            <!-- Notification Button -->
+            {{-- <button class="btn btn-ghost btn-circle">
+                <div class="indicator">
+                    <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" fill="none" viewBox="0 0 24 24"
+                        stroke="currentColor">
+                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                            d="M15 17h5l-1.405-1.405M19 13V8a7 7 0 1 0-14 0v5l-1.405 1.595A1 1 0 0 0 5 18h14a1 1 0 0 0 .405-1.595L19 13z" />
                     </svg>
-                </button> --}}
-            </div>
-            <div class="mt-3 space-y-1 px-2">
-                <a href="#"
-                    class="block rounded-md px-3 py-2 text-base font-medium text-gray-400 hover:bg-gray-700 hover:text-white">Your
-                    Profile</a>
-                <a href="#"
-                    class="block rounded-md px-3 py-2 text-base font-medium text-gray-400 hover:bg-gray-700 hover:text-white">Settings</a>
-                <a href="#"
-                    class="block rounded-md px-3 py-2 text-base font-medium text-gray-400 hover:bg-gray-700 hover:text-white">Sign
-                    out</a>
+                    <span class="badge badge-xs badge-primary indicator-item"></span>
+                </div>
+            </button> --}}
+            <!-- Profile Dropdown -->
+            <div class="dropdown dropdown-end">
+                <div tabindex="0" role="button" class="btn btn-ghost btn-circle avatar">
+                    <div class="w-10 rounded-full">
+                        <img alt="User" src="https://api.dicebear.com/9.x/notionists/svg?seed=Ave-chan" />
+                    </div>
+                </div>
+                <ul tabindex="0"
+                    class="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52">
+                    <li><a href="#">Profile</a></li>
+                    <li><a href="#">Settings</a></li>
+                    <li><a href="#">Logout</a></li>
+                </ul>
             </div>
         </div>
     </div>
